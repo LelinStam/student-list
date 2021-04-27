f15d0426837d2d242fe320be8b1bdb2a7c2d7e4d
Lisa Clemens <34919562+LelinStam@users.noreply.github.com>
2021-04-27 15:12:24 -0500

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: ""
+  , component: HomeComponent
+}
+,{
+  path: "add-student"
+  , component: AddStudentComponent
+}
+, {
+  path: "**"
+  , component: PageNotFoundComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
